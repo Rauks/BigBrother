8632312094de0c4324b8a04623e605fdf19e737f
Karl Woditsch <k.woditsch@gmail.com>
2013-11-13 16:50:12 +0100

    Add handlers open, scan.

--- BigBrother-Gui/src/bigbrother/gui/BigBrotherGuiController.java
+++ BigBrother-Gui/src/bigbrother/gui/BigBrotherGuiController.java
@@ -7,7 +7,10 @@
 package bigbrother.gui;
 
 import bigbrother.core.Scanner;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -17,8 +20,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  * FXML Controller class
@@ -27,28 +34,56 @@ import javafx.scene.control.TreeView;
  */
 public class BigBrotherGuiController implements Initializable {
     @FXML
+    public Pane rootPane;
+    @FXML
     public ListView classesList;
-    
     @FXML
     public TreeView classesData;
+    
+    private FileChooser jarFileChooser;
+    private ObservableList<Class> observablesClasses;
 
+    @FXML
+    public void handleOpen(){
+        File jarFile = this.jarFileChooser.showOpenDialog(this.getScene().getWindow());
+        this.doScan(jarFile.getPath());
+    }
+    
+    public void doScan(String jarFilePath){
+        this.observablesClasses.clear();
+        try {
+            Scanner scanner = new Scanner(jarFilePath);
+            this.observablesClasses.addAll(scanner.getClasses());
+        } catch (IOException ex) {
+            Logger.getLogger(BigBrotherGuiController.class.getName()).log(Level.WARNING, null, ex);
+        }
+    }
+    
+    @FXML
+    public void handleClose(){
+        System.exit(0);
+    }
+    
+    protected Scene getScene(){
+        return this.rootPane.getScene();
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        try {
-            Scanner scanner = new Scanner("test-jars/TP-AIHM.jar");
-
-            List<Class> classes = scanner.getClasses();
-            ObservableList<Class> observablesClasses = FXCollections.observableList(classes);
-            this.classesList.setItems(observablesClasses);
-            for(Class classe : classes){
-                System.out.println(classe);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(BigBrotherGuiController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.jarFileChooser = new FileChooser();
+        this.jarFileChooser.setTitle("Explorer...");
+        this.jarFileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
+        this.jarFileChooser.getExtensionFilters().add(
+                new ExtensionFilter("Executables Java", "*.jar")
+        );
+        
+        this.observablesClasses = FXCollections.observableArrayList();
+        this.classesList.setItems(this.observablesClasses);
     }    
     
 }
