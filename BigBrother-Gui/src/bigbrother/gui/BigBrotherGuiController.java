f5ce17f57d47f634da787def936bf0226a024bc0
Karl Woditsch <k.woditsch@gmail.com>
2013-11-13 11:34:03 +0100

    Improve main window

--- BigBrother-Gui/src/bigbrother/gui/BigBrotherGuiController.java
+++ BigBrother-Gui/src/bigbrother/gui/BigBrotherGuiController.java
@@ -6,9 +6,19 @@
 
 package bigbrother.gui;
 
+import bigbrother.core.Scanner;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TreeView;
 
 /**
  * FXML Controller class
@@ -16,13 +26,29 @@ import javafx.fxml.Initializable;
  * @author Karl
  */
 public class BigBrotherGuiController implements Initializable {
+    @FXML
+    public ListView classesList;
+    
+    @FXML
+    public TreeView classesData;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            Scanner scanner = new Scanner("test-jars/TP-AIHM.jar");
+
+            List<Class> classes = scanner.getClasses();
+            ObservableList<Class> observablesClasses = FXCollections.observableList(classes);
+            this.classesList.setItems(observablesClasses);
+            for(Class classe : classes){
+                System.out.println(classe);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(BigBrotherGuiController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
